b059792b84c33f027e8a60ebddfb7a0266c9c45b
Mike Cronce <floppie@quadra-tec.net>
2014-01-15 21:59:21 -0500

    wemineltc/wemineftc:  Fixed NULLs, they should have been empty strings

--- certifications/wemineftc-units.sql
+++ certifications/wemineftc-units.sql
@@ -1,49 +1,49 @@
 -- Insert all the permutations of hash units and their conversions {{{
 -- Hash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES 
-	('Hash', NULL, 0, NULL, NULL, NULL),
+	('Hash', '', 0, '', '', ''),
 	('Kilohash', 'kHash', 1, 'Hash', '/', '1000'),
 	('Megahash', 'MHash', 1, 'Hash', '/', '1000000'),
 	('Gigahash', 'GHash', 1, 'Hash', '/', '1000000000');
 
 -- Kilohash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Kilohash', '*', '1000'),
-	('Kilohash', 'kHash', 0, NULL, NULL, NULL),
+	('Hash', '', 1, 'Kilohash', '*', '1000'),
+	('Kilohash', 'kHash', 0, '', '', ''),
 	('Megahash', 'MHash', 1, 'Kilohash', '/', '1000'),
 	('Gigahash', 'GHash', 1, 'Kilohash', '/', '1000000');
 
 -- Megahash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Megahash', '*', '1000000'),
+	('Hash', '', 1, 'Megahash', '*', '1000000'),
 	('Kilohash', 'kHash', 1, 'Megahash', '*', '1000'),
-	('Megahash', 'MHash', 0, NULL, NULL, NULL),
+	('Megahash', 'MHash', 0, '', '', ''),
 	('Gigahash', 'GHash', 1, 'Megahash', '/', '1000000');
 
 -- Gigahash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Gigahash', '*', '1000000000'),
+	('Hash', '', 1, 'Gigahash', '*', '1000000000'),
 	('Kilohash', 'kHash', 1, 'Gigahash', '*', '1000000'),
 	('Megahash', 'MHash', 1, 'Gigahash', '*', '1000'),
-	('Gigahash', 'GHash', 0, NULL, NULL, NULL);
+	('Gigahash', 'GHash', 0, '', '', '');
 -- }}}
 
 -- Insert all the permutations of FTC units and their conversions {{{
 -- FTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Feathercoin', 'FTC', 0, NULL, NULL, NULL),
+	('Feathercoin', 'FTC', 0, '', '', ''),
 	('Millifeathercoin', 'mFTC', 1, 'Feathercoin', '*', '1000'),
 	('Microfeathercoin', 'μFTC', 1, 'Feathercoin', '*', '1000000');
 
 -- mFTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Millifeathercoin', 'mFTC', 0, NULL, NULL, NULL),
+	('Millifeathercoin', 'mFTC', 0, '', '', ''),
 	('Feathercoin', 'FTC', 1, 'Millifeathercoin', '/', '1000'),
 	('Microfeathercoin', 'μFTC', 1, 'Millifeathercoin', '*', '1000');
 
 -- μFTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Microfeathercoin', 'μFTC', 0, NULL, NULL, NULL),
+	('Microfeathercoin', 'μFTC', 0, '', '', ''),
 	('Feathercoin', 'FTC', 1, 'Microfeathercoin', '/', '1000000'),
 	('Millifeathercoin', 'mFTC', 1, 'Microfeathercoin', '/', '1000');
 -- }}}
--- certifications/wemineltc-units.sql
+++ certifications/wemineltc-units.sql
@@ -1,49 +1,49 @@
 -- Insert all the permutations of hash units and their conversions {{{
 -- Hash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 0, NULL, NULL, NULL),
+	('Hash', '', 0, '', '', ''),
 	('Kilohash', 'kHash', 1, 'Hash', '/', '1000'),
 	('Megahash', 'MHash', 1, 'Hash', '/', '1000000'),
 	('Gigahash', 'GHash', 1, 'Hash', '/', '1000000000');
 
 -- Kilohash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Kilohash', '*', '1000'),
-	('Kilohash', 'kHash', 0, NULL, NULL, NULL),
+	('Hash', '', 1, 'Kilohash', '*', '1000'),
+	('Kilohash', 'kHash', 0, '', '', ''),
 	('Megahash', 'MHash', 1, 'Kilohash', '/', '1000'),
 	('Gigahash', 'GHash', 1, 'Kilohash', '/', '1000000');
 
 -- Megahash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Megahash', '*', '1000000'),
+	('Hash', '', 1, 'Megahash', '*', '1000000'),
 	('Kilohash', 'kHash', 1, 'Megahash', '*', '1000'),
-	('Megahash', 'MHash', 0, NULL, NULL, NULL),
+	('Megahash', 'MHash', 0, '', '', ''),
 	('Gigahash', 'GHash', 1, 'Megahash', '/', '1000000');
 
 -- Gigahash base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Hash', NULL, 1, 'Gigahash', '*', '1000000000'),
+	('Hash', '', 1, 'Gigahash', '*', '1000000000'),
 	('Kilohash', 'kHash', 1, 'Gigahash', '*', '1000000'),
 	('Megahash', 'MHash', 1, 'Gigahash', '*', '1000'),
-	('Gigahash', 'GHash', 0, NULL, NULL, NULL);
+	('Gigahash', 'GHash', 0, '', '', '');
 -- }}}
 
 -- Insert all the permutations of LTC units and their conversions {{{
 -- LTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Litecoin', 'LTC', 0, NULL, NULL, NULL),
+	('Litecoin', 'LTC', 0, '', '', ''),
 	('Millilitecoin', 'mLTC', 1, 'Litecoin', '*', '1000'),
 	('Microlitecoin', 'μLTC', 1, 'Litecoin', '*', '1000000');
 
 -- mLTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Millilitecoin', 'mLTC', 0, NULL, NULL, NULL),
+	('Millilitecoin', 'mLTC', 0, '', '', ''),
 	('Litecoin', 'LTC', 1, 'Millilitecoin', '/', '1000'),
 	('Microlitecoin', 'μLTC', 1, 'Millilitecoin', '*', '1000');
 
 -- μLTC base
 INSERT IGNORE INTO unitinfo (name, abbreviation, conversion, conversion_base, conversion_operation, conversion_value) VALUES
-	('Microlitecoin', 'μLTC', 0, NULL, NULL, NULL),
+	('Microlitecoin', 'μLTC', 0, '', '', ''),
 	('Litecoin', 'LTC', 1, 'Microlitecoin', '/', '1000000'),
 	('Millilitecoin', 'mLTC', 1, 'Microlitecoin', '/', '1000');
 -- }}}
